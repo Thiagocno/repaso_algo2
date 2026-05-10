aeae7f62274653c76ecdfb9f9477b6e71a882550
Thiago Cornes <thiagocornes06@gmail.com>
2026-05-10 13:25:31 -0300

    Arreglado error de test

--- src/test/kotlin/PerroTest.kt
+++ src/test/kotlin/PerroTest.kt
@@ -1,10 +1,12 @@
+package ar.edu.unsam.algo2
+
 import ar.edu.unsam.algo2.Perro
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
 class PerroTest : DescribeSpec ({
   describe("Tests de perro"){
-      it(""){
+      it("Perro dice guau"){
           val perro = Perro()
           perro.ladrar() shouldBe "guau"
       }
